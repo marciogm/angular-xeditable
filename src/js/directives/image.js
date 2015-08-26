d025f0f895a829157a6a482726afc9c06bee76a0
Marcio Giaxa Marinheiro <marciogiaxa@gmail.com>
2015-08-26 15:01:23 -0300

    Accept attribute for ngImgCrop
    Using e-* syntax.
    eg. e-area-type=“square”
    
    For all attributes -> https://github.com/alexk111/ngImgCrop#Options

--- src/js/directives/image.js
+++ src/js/directives/image.js
@@ -11,8 +11,19 @@ angular.module('xeditable').directive('editableImage', ['editableDirectiveFactor
         this.parent.render.call(this);
 
         var divInputFile   = angular.element('<div>Select an image file: <input type="file" id="fileInput" /></div>');
-        var divCropArea    = angular.element('<div class="cropArea"><img-crop image="myImage" result-image="$data"></img-crop></div>');
+        var divCropArea    = angular.element('<div class="cropArea"><img-crop id="fileCropArea" image="myImage" result-image="$data"></img-crop></div>');
         var divCroppedARea = angular.element('<div>Cropped Image:</div><div><img ng-src="{{$data}}" /></div>');
+
+        divCropArea.find("#fileCropArea").attr('change-on-fly',         this.attrs.eChangeOnFly);
+        divCropArea.find("#fileCropArea").attr('area-type',             this.attrs.eAreaType);
+        divCropArea.find("#fileCropArea").attr('area-min-size',         this.attrs.eAreaMinSize);
+        divCropArea.find("#fileCropArea").attr('result-image-size',     this.attrs.eResultImageSize);
+        divCropArea.find("#fileCropArea").attr('result-image-format',   this.attrs.eResultImageFormat);
+        divCropArea.find("#fileCropArea").attr('result-image-quality',  this.attrs.eResultImageQuality);
+        divCropArea.find("#fileCropArea").attr('on-change',             this.attrs.eOnChange);
+        divCropArea.find("#fileCropArea").attr('on-load-begin',         this.attrs.eOnLoadBegin);
+        divCropArea.find("#fileCropArea").attr('on-load-done',          this.attrs.eOnLoadDone);
+        divCropArea.find("#fileCropArea").attr('on-load-error',         this.attrs.eOnLoadError);
 
         divInputFile.find("#fileInput").on('change', this.scope.handleFileSelect);
 
